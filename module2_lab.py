3c2d971923f5f95f5810fdca1539d762654e885e
Luis Velasquez <luis.velasquezramirez@fwcsstudents.org>
2024-09-05 12:36:21 -0400

    The first version of the scholarship checker

--- module2_lab.py
+++ module2_lab.py
@@ -1,2 +1,52 @@
 # Luis Velasquez | module2_lab.py | Classify students based on their current grades
 
+# Contains the students, sorted by their respective list
+students = {}
+
+while True:
+    firstname = ""
+    lastname = ""
+    gpa = 0.0
+
+    # Makes sure only letters are entered as a last name
+    while True:
+        lastname = input("Lastname <<== ")
+        for char in lastname:
+            if not char.isalpha():
+                print(f"Sorry, invalid character: {char}")
+                break
+            else:
+                pass
+        else:
+            if lastname == "ZZZ":
+                quit()
+            break
+
+    # Makes sure only letters are entered as a first name
+    while True:
+        firstname = input("Firstname <<== ")
+        for char in firstname:
+            if not char.isalpha():
+                print(f"Sorry, invalid character: {char}")
+                break
+            else:
+                pass
+        else:
+            break
+
+    if lastname.lower() in students.keys():
+        print(f"\n{lastname.capitalize()} {students[lastname]['firstname'].capitalize()}")
+        if students[lastname]["gpa"] >= 3.5:
+            print("You are in the Dean's scholarship list\n")
+        elif 3.25 <= students[lastname]["gpa"] < 3.5:
+            print("You are in the Honor Roll scholarship list\n")
+        else:
+            print("Your GPA is not strong enough yet, keep working hard!\n")
+    else:
+        gpa = float(input("GPA <<== "))
+        print("You have been added to our system, please sign in!\n")
+
+        students[lastname] = {
+            "firstname": firstname,
+            "gpa": gpa
+        }
